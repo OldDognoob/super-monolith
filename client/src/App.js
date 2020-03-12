fae6b23e04be294086cf9d6cfd65ab4bd3c900c5
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-03-12 16:23:24 +0200

    initial changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+function getBaseURL() {
+  return process.env.NODE_ENV === 'development'
+    ? 'http://localhost:5000'
+    : ''
+}
+
+
 function App() {
+  const [greeting, setGreeting] = useState("");
+
+  useEffect(() => {
+    fetch("api/welcome?name=myEmperor")
+      .then(res => res.json())
+      .then(greeting => setGreeting(greeting))
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hosted might Emperor on heroku </h1>
+      <header className="App-header">{greeting}</header>
     </div>
   );
 }
